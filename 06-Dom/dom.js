66c1f9df9d14dc626b1836cbba0d7835149258a7
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-04-26 10:15:59 -0300

    Seletores CSS no JS

--- 06-Dom/dom.js
+++ 06-Dom/dom.js
@@ -1,5 +1,9 @@
 /* Métodos/Funções de acesso e seleção
 
+
+// DOM é um modelo pelo qual a arvore do HTML é construida
+
+
 getElementById() -> seleciona um elemento pelo ID
 
 querySelector()
@@ -43,3 +47,34 @@ legenda.innerHTML = "<b>Legenda da imagem</b>"; //inner HTML alem de mudar o con
 titulo.style.textAlign = "center"; 
 titulo.style.color = "purple";
 titulo.style.backgroundImage = "linear-gradient(lightyellow, lightblue)";
+
+// Seletores CSS
+// first child
+// last child
+// nth-child(2) pra pegar o do meio
+
+const listaEditores = document.querySelector('#lista-editores');
+
+const ultimo = listaEditores.querySelector('li:last-child')
+const primeiro = listaEditores.querySelector ('li:first-child');
+const outro = listaEditores.querySelector('li:nth-child(2)');
+
+//outro.innerHTML = "<b>Deu ruim</b>";
+//console.error(ultimo)
+//outro.style.color = "red";
+
+ultimo.classList.add('destaque-item');
+
+// Adicionando o atributo target em todos os links da lista de referências
+//Selecionando todos os links contidos na lista ul
+const links = document.querySelectorAll('ul li a');
+console.log(links);
+// links [0].style.color = 'red';
+
+ // i = i+1
+for (let i = 0; i < links.length; i++) {
+    links[i].setAttribute('target', '_blank');
+}
+
+
+
