b2fe2bc390d17bdb768c032c7064d29e7a3943af
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-05-03 09:22:48 -0300

    Trocando fontes e cor

--- Ex02-Advinha/advinha.js
+++ Ex02-Advinha/advinha.js
@@ -9,16 +9,16 @@ formulario.addEventListener('submit', function(event) {
     let resultado = document.getElementById('resultado')
 
     if (palpiteUsuario == numeroEscolhido) {
-        resultado.innerHTML = "Você acertou!";
+        resultado.innerHTML = "Você acertou!";                              
     } else if (palpiteUsuario < 0 || palpiteUsuario > 10) {
         resultado.innerHTML = "Digite um número entre 0 e 10";
     } else if (numeroEscolhido > palpiteUsuario)  {
-        resultado.innerHTML = "seu palpite é maior que o número sorteado";
+        resultado.innerHTML = "Seu palpite é maior que o número sorteado";
         
     } else if (numeroEscolhido < palpiteUsuario) {
-        resultado.innerHTML = "seu palpite é menor que o número sorteado";
+        resultado.innerHTML = "Seu palpite é menor que o número sorteado";
     } else {
         resultado.innerHTML = "Erroouuuuu!";
   }
 
-});
+});  
